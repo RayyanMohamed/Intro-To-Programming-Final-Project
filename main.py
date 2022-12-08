3a7a974b0bb10acbbf944f08916ce2ce2c5b8d67
RayyanMohamed <119700825+RayyanMohamed@users.noreply.github.com>
2022-12-08 08:29:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -40,3 +40,102 @@ for i in range(len(df)):
     claim_label_list.append(claim_label)
     evidence_label_list.append(d["evidence_label"])
     votes_list.append(d["votes"])
+
+a = 0
+b = 0
+c = 0
+d = 0
+
+a1 = 0
+b1 = 0
+c1 = 0
+d1 = 0
+
+labels = []
+length = []
+
+supports = []
+refutes = []
+not_enough_info = []
+disputed = []
+
+for i in range(len(df)):
+  if(label[i] == 0):
+    # Supports
+    a = a + len(dataset["test"]["claim"][i])
+    a1 = a1 + 1
+    length.append(len(dataset["test"]["claim"][i]))
+    labels.append(0)
+    supports.append(len(dataset["test"]["claim"][i]))
+  if(label[i] == 1):
+    # Refutes
+    b = b + len(dataset["test"]["claim"][i])
+    b1 = b1 + 1
+    length.append(len(dataset["test"]["claim"][i]))
+    labels.append(1)
+    refutes.append(len(dataset["test"]["claim"][i]))
+  if(label[i] == 2):
+    # Not enough information
+    c = c + len(dataset["test"]["claim"][i])
+    c1 = c1 + 1
+    length.append(len(dataset["test"]["claim"][i]))
+    labels.append(2)
+    not_enough_info.append(len(dataset["test"]["claim"][i]))
+  if(label[i] == 3):
+    # Disputed
+    d = d + len(dataset["test"]["claim"][i])
+    d1 = d1 + 1
+    length.append(len(dataset["test"]["claim"][i]))
+    labels.append(3)
+    disputed.append(len(dataset["test"]["claim"][i]))
+
+print(a, ": ", a1)
+print(b, ": ", b1)
+print(c, ": ", c1)
+print(d, ": ", d1)
+
+print("Supports: ", 83754/654)
+print("Refutes: ", 27698/253)
+print("Not Enough Information: ", 59060/474)
+print("Disputed: ", 18909/154)
+
+plt.hist(supports, alpha = 0.5)
+plt.hist(not_enough_info, alpha = 0.5)
+plt.hist(disputed, alpha = 0.5)
+plt.hist(refutes, alpha = 0.5)
+
+plt.title('Frequency of Claim Length')
+plt.xlabel('Claim Length (# of characters)')
+plt.ylabel('Number of Occurences')
+
+legend = ['Supports', 'Refutes', 'NEI', 'Disputes']
+plt.legend(legend)
+
+plt.style.use('seaborn')
+plt.title('Claim Length vs. Claim Label')
+plt.xlabel('Claim Length (# of characters)')
+plt.ylabel('Claim Label (0, 1, 2, 3)')
+plt.scatter(length, labels, edgecolor = 'black', linewidth = 1, alpha = 0.2)
+
+# Create a data frame with claim_id, claim, claim_label, evidence_label, and votes
+data={'claim_id' : claim_id_list, 'claim' : claim_list, 'claim_label' : claim_label_list, 'evidence_label' : evidence_label_list, 'votes' : votes_list}
+df_evidence = pd.DataFrame(data=data)
+
+# Print first five rows of the data frame
+df_evidence.head()
+
+print(votes_list)
+
+df.labels.value_counts()
+
+# Create a table with claim_id and claim_label
+data_labels = {
+    'Claim ID' : claim_id,
+    'Claim Label' : label
+}
+data_table = pd.DataFrame(data_labels)
+data_table
+
+# Create new variable with length of claim text for each claim and plot
+claim_lens=[len(text) for text in df.claims.values]
+plt.hist(claim_lens)
